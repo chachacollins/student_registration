e1636fc483c68361e2ebf80ed85a6d28f6d403fa
chachacollins <collinschacha@hotmail.com>
2026-06-20 17:43:57 +0300

    initial commit

---
+++ flake.nix
@@ -0,0 +1,35 @@
+{
+  description = "student registration system";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = nixpkgs.lib.genAttrs supportedSystems;
+    in
+    {
+      devShells = forAllSystems (system:
+        let
+          pkgs = import nixpkgs { inherit system; };
+        in
+        {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              gnumake
+              pkg-config
+              gcc16
+            ];
+
+            buildInputs = with pkgs; [
+              crow
+              nlohmann_json
+              libpqxx
+            ];
+
+            shellHook = "";
+          };
+        });
+    };
+}
